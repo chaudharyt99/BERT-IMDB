0f56ee61587dbb8935cccf34bdd9a96e8bd71358
Tushar Choudhary <83606579+chaudharyt99@users.noreply.github.com>
2023-01-20 06:18:47 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,73 @@
+import config
+import torch
+import flask
+import time
+from flask import Flask
+from flask import request
+from flask import render_template
+from model import BERTBaseUncased
+import functools
+import torch.nn as nn
+
+
+app = Flask(__name__)
+
+MODEL = None
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+PREDICTION_DICT = dict() 
+
+
+def sentence_prediction(sentence):
+    tokenizer = config.TOKENIZER
+    max_len = config.MAX_LEN
+    review = str(sentence)
+    review = ' '.join(review.split())
+
+    inputs = tokenizer.encode_plus(
+        review, None, add_special_tokens=True, max_length=max_len
+    )
+
+    ids = inputs["input_ids"]
+    mask = inputs["attention_mask"]
+    token_type_ids = inputs["token_type_ids"]
+
+    ids = torch.tensor(ids, dtype=torch.long).unsqueeze(0)
+    mask = torch.tensor(mask, dtype=torch.long).unsqueeze(0)
+    token_type_ids = torch.tensor(token_type_ids, dtype=torch.long).unsqueeze(0)
+
+    ids = ids.to(DEVICE, dtype=torch.long)
+    token_type_ids = token_type_ids.to(DEVICE, dtype=torch.long)
+    mask = mask.to(DEVICE, dtype=torch.long)
+
+    outputs = MODEL(ids=ids, mask=mask, token_type_ids=token_type_ids)
+
+    outputs = torch.sigmoid(outputs).cpu().detach().numpy()
+    return outputs[0][0]
+
+
+@app.route("/", methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+@app.route("/predict")
+def predict():
+    sentence = request.args.get("sentence")
+    start_time = time.time()
+    positive_prediction = sentence_prediction(sentence)
+    negative_prediction = 1 - positive_prediction
+    response = {}
+    response["response"] = {
+        "positive": str(positive_prediction),
+        "negative": str(negative_prediction),
+        "sentence": str(sentence),
+        "time_taken": str(time.time() - start_time),
+    }
+    return flask.jsonify(response)
+
+
+if __name__ == "__main__":
+    MODEL = BERTBaseUncased()
+    MODEL.load_state_dict(torch.load(config.MODEL_PATH))
+    MODEL.to(DEVICE)
+    MODEL.eval()
+    app.run(host="0.0.0.0", port="9999", debug=True)
